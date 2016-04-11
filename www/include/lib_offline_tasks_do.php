bb084ca3bd1bbf8a0fe7f1ed9394f0fb40a569c9
thisisaaronland <aaron@mapzen.com>
2016-04-11 17:15:33 +0000

    updated comments

--- www/include/lib_offline_tasks_do.php
+++ www/include/lib_offline_tasks_do.php
@@ -2,6 +2,10 @@
 
 	# $GLOBALS['offline_tasks_handlers'] is defined in lib_offline_tasks.php
 
+	# Given that we are being explicit about function names in lib_offline_tasks
+	# (offline_tasks_function_name) it's not clear why or what benefit spelling
+	# them out here gets us. But today, we do... (20160411/thisisaaronland)
+
 	########################################################################
 
 	$GLOBALS['offline_tasks_handlers']['example'] = 'offline_tasks_do_example';
